997ca4b86cbb9d0b5a004ea39815673dfb8efef5
Abhijit Pandey <abhijitpandey524@gmail.com>
2024-06-08 19:39:09 +0530

    rate limiting

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,8 +1,13 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
 
+	"example.com/database"
+
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -28,7 +33,24 @@ func ShortenURL(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"cannot parse JSON"})
 	}
 
-	//implement rate limiting  
+	//implement rate limiting 
+	
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+	val, err := r2.Get(database.Ctx, c.IP()).Result()
+	if err == redis.Nil {
+		_ = r2.Set(database.Ctx , c.IP() , os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else{
+		val, _ := r2.Get(database.Ctx , c.IP()).Result()
+		valInt, _ := strconv.Atoi(val)
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx , c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error": "rate limit exccedded",
+				"rate_limit_rest": limit / time.Nanosecond / time.Minute,
+			})
+		}
+	}
 
 	//check of the input is an actual url
 
@@ -43,6 +65,8 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	//enforce https, SSL
 	body.URL = helper.EnforceHTTP(body.URL)
+
+	r2.Decr(database.Ctx , c.IP())
 
 
 
